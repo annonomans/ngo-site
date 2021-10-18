60dd82d6dbacb1bd33adcb2526e710100a83e842
annonomans <infotonoman@gmail.com>
2021-10-18 17:17:41 +0530

    service-worker

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './Header';
 function App() {
   return (
     <Header />
+
+    
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 // import logo from './logo.svg';
 import './App.css';
+// import { Route, Switch, Link } from "react-router-dom";
 
 class Header extends Component {
 
 	render() {
         const site_title = "Ngo-site"
     	return (	
-    		<h1>Hello, {site_title}</h1>
+
+            <div>
+                <h2>Hello , {site_title}</h2>
+                <nav>
+                    <ul id="navigation">
+                        <li>
+                        <h3>Home</h3>
+                        </li>
+                        <li>
+                        <h3>About</h3>
+                        </li>
+                        <li>
+                        <h3>Contact</h3>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
     		
     		);
   	}
